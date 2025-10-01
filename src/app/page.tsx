b39d33e9f8ffa6cba9ccf648939e0709baafba65
Hernan Medrano <hmvilladiego@gmail.com>
2025-10-01 03:12:21 -0500

    Pruebas css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,35 @@
 "use client";
 
 import { useState } from "react";
-import { AppBar, Tabs, Tab, Box, Typography } from "@mui/material";
+import { Tab, Box} from "@mui/material";
 import Dashboard from "@/components/Dashboard";
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
-function TabPanel({
-  children,
-  value,
-  index,
-}: {
-  children: React.ReactNode;
-  value: number;
-  index: number;
-}) {
-  return (
-    <div hidden={value !== index}>
-      {value === index && <Box p={2}>{children}</Box>}
-    </div>
-  );
-}
 
 export default function HomePage() {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState('1');
 
-  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
 
-  return (
-    <Box>
-      <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Dashboard" />
-          <Tab label="Tab 2" />
-          <Tab label="Tab 3" />
-          <Tab label="Tab 4" />
-        </Tabs>
-      </AppBar>
-
-      <TabPanel value={value} index={0}>
-        <Dashboard />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Typography>Contenido del Tab 2</Typography>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Typography>Contenido del Tab 3</Typography>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <Typography>Contenido del Tab 4</Typography>
-      </TabPanel>
+   return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', bgcolor: '#93959767' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Dashboard" value="1" />
+            <Tab label="Item Two" value="2" />
+            <Tab label="Item Three" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1"><Dashboard/></TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
     </Box>
   );
 }
+ 
